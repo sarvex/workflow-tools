6c1bfe56980007dba7b2eb8d398a14f1efb2b68d
Sarvex Jatasra <sarvex.jatasra@gmail.com>
2023-02-22 20:45:42 +0530

    Initial commit

---
+++ exercises/Ch 2/02_05/autocomplete.js
@@ -0,0 +1,7 @@
+/* 
+Generate an if else statement just by typing "ifelse" + tab or enter. 
+
+Learn more about code completion for VS code here:
+https://code.visualstudio.com/docs/editor/intellisense
+
+/* 
